6f08256b1acce045adf13d2d6480dba4bc778985
Kaiser-Kharsair <cjy2951042633@gmail.com>
2022-09-15 19:01:51 +1000

    broken od

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+
+
 def main():
     import krpc
     conn = krpc.connect(
@@ -6,9 +9,31 @@ def main():
         rpc_port=50000,
         stream_port=50001
     )
+
     vessel = conn.space_center.active_vessel
+    from maths.vec3d import l2_norm, Vec3D
+    vessel_position = vessel.position(vessel.orbit.body.non_rotating_reference_frame)
+    x_axis_visual_end = (l2_norm(Vec3D(vessel_position[0], vessel_position[1], vessel_position[2])), 0, 0)
+
+    conn.drawing.add_direction(x_axis_visual_end, vessel.orbital_reference_frame)
+    print(x_axis_visual_end)
+
     print(vessel.name)
     print('({})'.format(vessel.position(vessel.orbit.body.reference_frame)))
+    from misc.preliminary_orbit_determination import od_from_position_and_velocity
+    orbital_elements = od_from_position_and_velocity(vessel.position(vessel.orbit.body.non_rotating_reference_frame),
+                                                     vessel.velocity(vessel.orbit.body.non_rotating_reference_frame),
+                                                     0, vessel)
+    print("Semi-Major Axis (km): {}, Eccentricity: {}, Inclination: {}, LAN: {}, ARG Pe: {}, Ture Anomaly: {}".format(orbital_elements[0] / 1000000
+                                                                                                                 , orbital_elements[1]
+                                                                                                                 , orbital_elements[2]
+                                                                                                                 , orbital_elements[3]
+                                                                                                                 , orbital_elements[4]
+                                                                                                                 , orbital_elements[5]))
+
+    while True:
+        pass
+
 
 
 if __name__ == '__main__':
--- misc/preliminary_orbit_determination.py
+++ misc/preliminary_orbit_determination.py
@@ -1,17 +1,20 @@
 import krpc
-from krpc.SpaceCenter import SpaceCenter
 from maths import vec3d
 from maths.vec3d import Vec3D
 from math import acos
 
 
 def od_from_position_and_velocity(position: tuple[float, float, float], velocity: tuple[float, float, float],
-                                  time: float, vessel: SpaceCenter.Vessel):
+                                  time: float, vessel):
     parent_body = vessel.orbit.body
     mu = parent_body.gravitational_parameter
 
     velocity = Vec3D(velocity[0], velocity[1], velocity[2])
     position = Vec3D(position[0], position[1], position[2])
+
+    kci_z_axis = Vec3D(0, 0, 1)
+    kci_x_axis = Vec3D(1, 0, 0)
+    kci_y_axis = Vec3D(0, 1, 0)
 
     v = vec3d.l2_norm(velocity)
     r = vec3d.l2_norm(position)
@@ -22,6 +25,30 @@ def od_from_position_and_velocity(position: tuple[float, float, float], velocity
     eccentricity_vector = (vec3d.vec_crs(velocity, angular_momentum_vector) / mu) - (position / r)
     eccentricity = vec3d.l2_norm(eccentricity_vector)
 
-    inclination = acos(vec3d.vec_dot(angular_momentum_vector, Vec3D(0, 1, 0)) / vec3d.l2_norm(angular_momentum_vector))
+    inclination = acos(vec3d.vec_dot(angular_momentum_vector, kci_y_axis) / vec3d.l2_norm(angular_momentum_vector))
 
-    return semi_major_axis, eccentricity, inclination
+    n_vector = vec3d.vec_crs(kci_y_axis, angular_momentum_vector)
+    longitude_of_ascending_node = acos(vec3d.vec_dot(kci_x_axis, n_vector) / vec3d.l2_norm(n_vector))
+
+    if vec3d.vec_dot(n_vector, kci_z_axis) >= 0:
+        print("0 <= LAN <= pi")
+    elif vec3d.vec_dot(n_vector, kci_z_axis) < 0:
+        print("pi < LAN < 2pi")
+
+    argument_of_periapsis = acos(vec3d.vec_dot(n_vector, eccentricity_vector) / (vec3d.l2_norm(n_vector) * eccentricity))
+
+    if vec3d.vec_dot(eccentricity_vector, kci_y_axis) >= 0:
+        print("0 <= Arg Pe <= pi")
+    elif vec3d.vec_dot(eccentricity_vector, kci_y_axis) < 0:
+        print("pi < Arg Pe < 2pi")
+
+    argument_of_latitude = acos(vec3d.vec_dot(n_vector, position) / (vec3d.l2_norm(n_vector) * r))
+    print(argument_of_latitude)
+    if vec3d.vec_dot(position, kci_y_axis) >= 0:
+        print("0 <= u <= pi")
+    elif vec3d.vec_dot(position, kci_y_axis) < 0:
+        print("pi < u  < 2pi")
+
+    true_anomaly = argument_of_latitude + argument_of_periapsis
+
+    return semi_major_axis, eccentricity, inclination, longitude_of_ascending_node, argument_of_periapsis, true_anomaly
